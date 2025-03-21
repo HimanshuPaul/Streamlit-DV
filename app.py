73a49029347d38254f3de8ac9f37eb442f6db04b
HimanshuPaul <h1503paul@gmail.com>
2025-03-21 12:09:10 +0530

    modified main.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+st.title('Exploratory Data Analysis')
+
+
+# st.title("dv")
+st.set_page_config(page_title="dv",
+	page_icon="🤣")
+st.title("data visualization with streamit")
+with st.sidebar:
+	uplode=st.file_uploader("uplodde csv")
+if uplode is not None:
+	df=pd.read_csv(uplode)
+	st.dataframe(df.head())
